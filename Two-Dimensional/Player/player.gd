1e704ab6ced4a2cfaa2caadbf9bca4e857d309d1
Julian <julian@wolfjawstudios.com>
2024-02-09 21:37:56 -0500

    Updated player/wizard to use AnimationTree logic. Setting up new movement logic.

--- Two-Dimensional/Player/player.gd
+++ Two-Dimensional/Player/player.gd
@@ -8,19 +8,29 @@ const JUMP_VELOCITY = -400.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+var anim_tree
+var anim_state
+
 @export var health = 100
+var dead = false
+
+var is_moving_left = false
+var is_moving_right = false
+var is_jumping = false
+var is_attacking = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	screen_size = get_viewport_rect().size
+	anim_state = anim_tree.get("parameters/playback")
 	pass # Replace with function body.
 
 func flip_right():
-	$Sprite2D.flip_h = false
+	scale.x = 1
 	
 func flip_left():
-	$Sprite2D.flip_h = true
-
+	scale.x = -1
+	
 func _physics_process(delta):
 	# Check for Game Over / Death
 	if health <= 0:
@@ -41,17 +51,17 @@ func _physics_process(delta):
 	if direction:
 		velocity.x = direction * speed
 		if Input.is_action_pressed("attack1"):
-			$AnimationPlayer.play("attack1")
-		elif Input.is_action_pressed("attack2"):
-			$AnimationPlayer.play("attack2")
-		elif Input.is_action_pressed("special1"):
-			$AnimationPlayer.play("special1")	
-		elif Input.is_action_pressed("special2"):
-			$AnimationPlayer.play("special2")
+			anim_state.travel("attack1")
+		#elif Input.is_action_pressed("attack2"):
+			#anim_state.travel("attack2")
+		#elif Input.is_action_pressed("special1"):
+			#anim_state.travel("special1")	
+		#elif Input.is_action_pressed("special2"):
+			#anim_state.travel("special2")
 		elif Input.is_action_pressed("run"):
-			$AnimationPlayer.play("run")
+			anim_state.travel("run")
 		else:
-			$AnimationPlayer.play("walk")
+			anim_state.travel("walk")
 		if (velocity.x < 0):
 			flip_left()
 		else:
@@ -59,15 +69,15 @@ func _physics_process(delta):
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed)
 		if Input.is_action_pressed("attack1"):
-			$AnimationPlayer.play("attack1")
-		elif Input.is_action_pressed("attack2"):
-			$AnimationPlayer.play("attack2")
-		elif Input.is_action_pressed("special1"):
-			$AnimationPlayer.play("special1")	
-		elif Input.is_action_pressed("special2"):
-			$AnimationPlayer.play("special2")	
+			anim_state.travel("attack1")
+		#elif Input.is_action_pressed("attack2"):
+			#anim_state.travel("attack2")
+		#elif Input.is_action_pressed("special1"):
+			#anim_state.travel("special1")	
+		#elif Input.is_action_pressed("special2"):
+			#anim_state.travel("special2")	
 		else:
-			$AnimationPlayer.play("idle")
+			anim_state.travel("idle")
 
 	move_and_slide()
 
@@ -75,6 +85,12 @@ func _on_hit_box_area_entered(area):
 	if area.is_in_group("hurtbox"):
 		area.take_damage()
 		
-func take_damage(damage):
-	health -= damage
+func take_damage(damageValue):
+	health -= damageValue
 	$AnimationPlayer.play("hurt")
+
+
+func _on_animation_player_animation_finished(anim_name):
+	if (anim_name == "attack1"):
+		is_attacking = false
+	pass # Replace with function body.
